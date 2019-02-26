c05cf3fdbb5e3d5e6898f845bc97637683e087bc
Tarma <tarmakatz@gmail.com>
2019-02-26 11:51:25 +0100

    Added responsiveMargins mixin

--- styles/abstract/_mixins.tsx
+++ styles/abstract/_mixins.tsx
@@ -1,3 +1,13 @@
+import { respond } from './_functions';
+
 export const flexCenterCenter = () => {
   return `display: flex; justify-content: center; align-items: center;`;
+};
+
+export const responsiveMargins = () => {
+  return `
+    width: 90%; 
+    margin: 0 auto; 
+    ${respond('tab-land', 'width: 70%')}
+  `;
 };
