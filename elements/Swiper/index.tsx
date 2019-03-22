29dfcb5d5756cad16b1a79f795a2b806afc1aaff
Thomas Tränkler <github.com@thomas.traenkler.com>
2019-03-22 14:36:48 +0100

    - added swiper animation example for react-spring - workaround for buggy server DONE_TASK - missing action type 4 logfile parsing lookup

---
+++ elements/Swiper/index.tsx
@@ -0,0 +1,48 @@
+import React, { ReactNode } from "react";
+import ReactDOM from "react-dom";
+import { useSpring, animated, interpolate } from "react-spring";
+import { useGesture } from "react-with-gesture";
+import "./styles.css";
+
+function Slider({ children }: { children: ReactNode }) {
+  const [bind, { delta, down }] = useGesture();
+  const { x, bg, size } = useSpring({
+    x: down ? delta[0] : 0,
+    bg: `linear-gradient(120deg, ${
+      delta[0] < 0 ? "#f093fb 0%, #f5576c" : "#96fbc4 0%, #f9f586"
+    } 100%)`,
+    size: down ? 1.1 : 1,
+    immediate: name => down && name === "x"
+  });
+  const avSize = x.interpolate({
+    // @ts-ignore
+    map: Math.abs,
+    range: [50, 300],
+    output: ["scale(0.5)", "scale(1)"],
+    extrapolate: "clamp"
+  });
+  return (
+    <animated.div {...bind()} className="item" style={{ background: bg }}>
+      <animated.div
+        className="av"
+        style={{
+          transform: avSize,
+          justifySelf: delta[0] < 0 ? "end" : "start"
+        }}
+      />
+      <animated.div
+        className="fg"
+        style={{
+          transform: interpolate(
+            [x, size],
+            (x, s) => `translate3d(${x}px,0,0) scale(${s})`
+          )
+        }}
+      >
+        {children}
+      </animated.div>
+    </animated.div>
+  );
+}
+
+ReactDOM.render(<Slider>Slide.</Slider>, document.getElementById("root"));
